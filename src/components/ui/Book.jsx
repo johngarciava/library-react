f15c78925841b4875f6a741f2b2d9e67ff5443d7
John <jgarcia51196@gmail.com>
2022-07-11 21:11:56 -0400

    removed ref

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router-dom"
 import Rating from "./Rating"
